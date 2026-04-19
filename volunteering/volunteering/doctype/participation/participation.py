59a82e1bbdaedb79efd185c3ad541ec387602581
AthletiCoder <varunkumar.nagandla@gmail.com>
2026-04-19 18:26:50 +0530

    updated reg form

--- volunteering/volunteering/doctype/participation/participation.py
+++ volunteering/volunteering/doctype/participation/participation.py
@@ -31,19 +31,3 @@ class Participation(Document):
         
         # 3. Map the link field
         self.volunteer = v_name
-
-    def get_permission_query_conditions(user):
-        if not user: user = frappe.session.user
-        
-        roles = frappe.get_roles(user)
-        if "NGO Admin" in roles or "NGO Coordinator" in roles:
-            return ""
-
-        if "NGO Member" in roles:
-            # Filter: Only show records where the linked volunteer's user_id matches the session user
-            return f"""
-                `tabParticipation`.volunteer IN (
-                    SELECT name FROM `tabVolunteer` WHERE user_id = {frappe.db.escape(user)}
-                )
-            """
-        return "1=0"
