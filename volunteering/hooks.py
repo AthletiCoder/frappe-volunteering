7217d1f9b1e8c1693d4c602a86a5db15b122227a
AthletiCoder <varunkumar.nagandla@gmail.com>
2026-04-30 12:46:20 +0530

    revert workspace changes

--- volunteering/hooks.py
+++ volunteering/hooks.py
@@ -266,4 +266,5 @@ has_permission = {
 fixtures = [
     {"dt": "Role", "filters": [["name", "in", ["NGO Admin", "NGO Coordinator", "NGO Member"]]]},
     {"dt": "Web Form", "filters": [["module", "=", "Volunteering"]]},
+    {"dt": "Workspace", "filters": [["module", "=", "Volunteering"]]}
 ]
